adb4bff5c08198aa58014b46fd05b0deac10547a
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-11-19 13:27:07 -0500

    added favicon, added models folder

--- containers/App.js
+++ containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Head from 'next/head'
 import Login from '~/components/Login'
 import Navbar from '~/components/Navbar'
+import Favicon from '~/assets/favicon.ico'
 
 export default class App extends Component {
     constructor(props){
@@ -12,6 +13,7 @@ export default class App extends Component {
             <>
             <Head>
                 <title>Client Connect</title>
+                <link rel="icon" type="image/ico" href={Favicon}></link>
                 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
             </Head>
             <div className="App">
