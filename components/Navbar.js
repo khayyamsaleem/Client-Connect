f4397def94ee8e1681d31eef07c2faa172480a04
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-11-25 16:11:59 -0500

    navbar renders conditionally

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
 import { Menu } from 'semantic-ui-react'
+import { getFromStorage } from '~/utils/storage'
+import { logoutUser } from '~/utils/api/users'
+import Router from 'next/router'
 
 export default class Navbar extends Component {
     render() {
@@ -9,14 +12,32 @@ export default class Navbar extends Component {
                 <Link href='/'>
                     <Menu.Item header as="h2">Client Connect</Menu.Item>
                 </Link>
+                {this.props.loggedIn  ? (
+                <Menu.Menu position="right">
+                    <Link href="/profile">
+                        <Menu.Item as="a" content="Profile"></Menu.Item>
+                    </Link>
+                    <Menu.Item as="a" content="Logout" onClick={async () => {
+                        const obj = getFromStorage('clientconnect')
+                        if (obj && obj.token){
+                            const { token } = obj
+                            const res = await logoutUser(token)
+                            if (res.success) {
+                                Router.push('/')
+                            }
+                        }
+                    }}></Menu.Item>
+                </Menu.Menu>
+                ) : (
                 <Menu.Menu position="right">
                     <Link href='/login'>
-                        <Menu.Item as="a" content="Login" />
+                       <Menu.Item as="a" content="Login" />
                     </Link>
                     <Link href='/register'>
                         <Menu.Item as="a" content="Register" />
                     </Link>
                 </Menu.Menu>
+                )}
             </Menu>
         );
     }
--- containers/App.js
+++ containers/App.js
@@ -3,8 +3,29 @@ import Head from 'next/head'
 import Navbar from '~/components/Navbar'
 import Favicon from '~/assets/favicon.ico'
 import "~/styles/App.scss"
+import { getFromStorage} from '~/utils/storage'
+import { verifyToken } from '~/utils/api/users'
 
 export default class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            loggedIn: false
+        }
+    }
+    async componentDidMount() {
+        const tokenObj = getFromStorage('clientconnect');
+        if (tokenObj && tokenObj.token) {
+            const { token } = tokenObj;
+            // Verify token
+            const res = await verifyToken(token)
+            if (res.success){
+                this.setState({
+                    loggedIn: true
+                })
+            }
+        }
+    }
     render() {
         return (
             <>
@@ -14,7 +35,7 @@ export default class App extends Component {
                     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
                 </Head>
                 <div className="App">
-                    <Navbar />
+                    <Navbar loggedIn={this.state.loggedIn}/>
                     {this.props.children}
                 </div>
             </>
