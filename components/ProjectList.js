4f8066761f6a7f5b66e3e124db29b54efa71f15e
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-12-15 21:27:05 -0500

    fixed project list modal

--- components/ProjectList.js
+++ components/ProjectList.js
@@ -7,7 +7,8 @@ export default class ProjectList extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            projects: []
+            projects: [],
+            modalOpen: false
         }
     }
     componentDidMount = async () => {
@@ -17,18 +18,21 @@ export default class ProjectList extends Component {
         const projects = projectsObj.projects
         this.setState({ projects })
     }
+    handleOpen = () => this.setState({ modalOpen: true })
+    handleClose = () => this.setState({ modalOpen: false })
     render() {
         const { projects } = this.state
         return (
             <Grid.Row>
                 <Grid.Column width={16}>
-                    <Modal trigger={<Segment textAlign="center"><Header as="h3" content="Find freelancers by selecting a project!" /></Segment>}>
+                    <Segment onClick={this.handleOpen} textAlign="center"><Header as="h3" content="Click here to select a project!" /></Segment>
+                    <Modal open={this.state.modalOpen} onClose={this.handleClose}>
                         <Grid verticalAlign="middle">
                             <Grid.Row>
                                 <Grid.Column>
                                     <Segment.Group>
                                         {(projects.length > 0) ? projects.map((project, i) => 
-                                            <Segment key={i} onClick={() => this.props.handleSelect(project)}>
+                                            <Segment key={i} onClick={() => this.props.handleSelect(project) && this.handleClose()}>
                                                 <Header as="h5" content={project.title} />
                                                 {project.description}
                                             </Segment>
